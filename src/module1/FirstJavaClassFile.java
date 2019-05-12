9b56123b4fff8e64ade808388726be8a0310082d
sparvathina1 <srinivas.parvathina@samiinfoltd.com>
2019-05-12 20:41:43 +0100

    commit

---
+++ src/module1/FirstJavaClassFile.java
@@ -0,0 +1,13 @@
+package module1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstJavaClassFile {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+
+	}
+
+}
